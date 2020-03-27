dd5bbdeb7679d1feeca724801843a80c9b5ba665
half cambodian hacker man <half-kh-hacker@hackery.site>
2020-03-27 11:28:29 +0000

    Support passing arguments to direct-render scripts

--- sipy/ext/direct_render.py
+++ sipy/ext/direct_render.py
@@ -1,7 +1,7 @@
 from importlib.machinery import SourceFileLoader
 from types import ModuleType
 
-def run(ctx, name):
+def run(ctx, name, *args, **kwargs):
   name_sans_py_ext = name[:-3]
 
   module_name = "__direct_render__." + name
@@ -10,8 +10,8 @@ def run(ctx, name):
   loader.exec_module(script_module)
 
   if "render_text" in dir(script_module):
-    ctx.write_text(name_sans_py_ext, script_module.render_text(ctx))
+    ctx.write_text(name_sans_py_ext, script_module.render_text(ctx, *args, **kwargs))
   elif "render_data" in dir(script_module):
-    ctx.write_data(name_sans_py_ext, script_module.render_data(ctx))
+    ctx.write_data(name_sans_py_ext, script_module.render_data(ctx, *args, **kwargs))
   else:
     raise Exception("Cannot run direct script: " + name + ". Is render_text or render_data defined?")
