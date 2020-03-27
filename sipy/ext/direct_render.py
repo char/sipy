f58552e21be88c74020cfc214895142ae933a3d7
half cambodian hacker man <half-kh-hacker@hackery.site>
2020-03-27 11:52:26 +0000

    Add optional 'out_name' to direct render

--- sipy/ext/direct_render.py
+++ sipy/ext/direct_render.py
@@ -1,8 +1,8 @@
 from importlib.machinery import SourceFileLoader
 from types import ModuleType
 
-def run(ctx, name, *args, **kwargs):
-  name_sans_py_ext = name[:-3]
+def run(ctx, name, out_name=None, *args, **kwargs):
+  out_name = out_name or name[:3]
 
   module_name = "__direct_render__." + name
   loader = SourceFileLoader(module_name, ctx.source_directory.joinpath(name).resolve())
@@ -10,8 +10,8 @@ def run(ctx, name, *args, **kwargs):
   loader.exec_module(script_module)
 
   if "render_text" in dir(script_module):
-    ctx.write_text(name_sans_py_ext, script_module.render_text(ctx, *args, **kwargs))
+    ctx.write_text(out_name, script_module.render_text(ctx, *args, **kwargs))
   elif "render_data" in dir(script_module):
-    ctx.write_data(name_sans_py_ext, script_module.render_data(ctx, *args, **kwargs))
+    ctx.write_data(out_name, script_module.render_data(ctx, *args, **kwargs))
   else:
     raise Exception("Cannot run direct script: " + name + ". Is render_text or render_data defined?")
