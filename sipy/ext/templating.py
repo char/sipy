7bb13ee40c9d4a4a39b37029962fc13eea1bcf00
half cambodian hacker man <half-kh-hacker@hackery.site>
2020-03-27 10:54:26 +0000

    Allow setting the base directory for templating
    This lets us use things like extend and include

--- sipy/ext/templating.py
+++ sipy/ext/templating.py
@@ -1,4 +1,13 @@
-from jinja2 import Template
+from jinja2 import Environment, BaseLoader, FileSystemLoader
+
+
+loader = BaseLoader()
+Template = lambda source: Environment(loader=loader).from_string(source)
+
+
+def set_base_directory(path: str):
+  global loader; loader = FileSystemLoader(path)
+
 
 def from_file(path: str) -> Template:
   with open(path) as f:
